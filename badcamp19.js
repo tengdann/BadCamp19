94576ab83f6c1cbbf1daf5a6b8aa9323a0003f0f
Danny Teng <tengdann@umich.edu>
2019-09-18 16:48:26 -0400

    Added drupal module stuff

---
+++ badcamp19.js
@@ -0,0 +1,98 @@
+// TODO: FIX
+    // Use jQuery to select ID w/ wildcard
+    /*
+    document.getElementById('edit-field-media-image-0-upload').addEventListener('change', function (event) {
+        ProcessImage();
+    }, false);
+    */
+
+    $(document).ready(function() {
+    $('input[id*="edit-field-upload-und-0-upload-button"]').change(function() {
+        ProcessImage();
+    });
+});
+
+// Calls DetectLabels API and provides formatted descriptor of image
+function DetectLabels(imageData) {
+    AWS.region = 'RegionToUse';
+    var rekognition = new AWS.Rekognition();
+    var params = {
+        Image: {
+            Bytes: imageData
+        },
+        MaxLabels: 5,
+        MinConfidence: 95,
+    };
+
+    rekognition.detectLabels(params, function(err, data) {
+        if (err) console.log(err, err.stack); //oopsie doopsie something went wrong
+        else {
+            var alt_text = 'Image may contain:'
+            for (var i = 0; i < data.Labels.length; i++) {
+                alt_text += ' ' + data.Labels[i].Name;
+            }
+            // TODO: FIX
+            // Need to use jQuery to find alt-tag w/ wildcard
+            //document.getElementById('edit-field-media-image-0-alt*').value = alt_text
+        }
+    });
+}
+
+// Loads selected image and unencodes image bytes for Rekognition DetectLabels API
+function ProcessImage() {
+    AnonLog();
+    // Find the actual file using JS/jQuery
+    var control = $('input[id*="edit-field-upload-und-0-upload-button"]');
+    var file = control.files[0];
+
+    // Load base64 encoded image
+    var reader = new FileReader();
+    reader.onload = (function (theFile) {
+        return function (e) {
+            var img = document.createElement('img');
+            var image = null;
+            img.src = e.target.result;
+            var jpg = true;
+            try {
+                image = atob(e.target.result.split("data:image/jpeg;base64,")[1]);
+            }
+            catch (e) {
+                jpg = false;
+            }
+            if (jpg == false) {
+                try {
+                    image = atob(e.target.result.split("data:image/png;base64,")[1]);
+                }
+                catch (e) {
+                    alert("Not an image file Rekognition can process");
+                }
+            }
+            var length = image.length;
+            imageBytes = new ArrayBuffer(length);
+            var ua = new Uint8Array(imageBytes);
+            for (var i = 0; i < length; i++) {
+                ua[i] = image.charCodeAt(i);
+            }
+            // Call Rekognition
+            DetectLabels(imageBytes)
+        };
+    })(file);
+    reader.readasDataURL(file);
+}
+
+// Provides anonymous log on to AWS services
+function AnonLog() {
+    // Configure the credentials provider to use your identity pool
+    AWS.config.region = 'RegionToUse';
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'IdentityPoolIdToUse',
+    });
+
+    // Make the call to obtain credentials
+    AWS.config.credentials.get(function () {
+        // Credentials will be available when this function is called
+        var accessKeyId = AWS.config.credentials.accessKeyId;
+        var secretAcessKey = AWS.config.credentials.secretAccessKey;
+        var sessionToken = AWS.config.credentials.sessionToken;
+    });
+}
